3b4feb98108fad2197f5746f836b26a1a28b5de7
jvdamgaard <jakob@jakobs-imac.home>
2014-03-30 20:59:50 +0200

    Responsive images

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 /* global $ */
 
 $(function() {
+
+    var $sections = $('.section');
+
+    // Initialize full page
     $.fn.fullpage({
         verticalCentered: true,
         menu: false,
@@ -8,6 +12,14 @@ $(function() {
         animateAnchor: false,
         scrollingSpeed: 500,
         fixedElements: '.menu, .logo, .email',
-        resize: false
+        resize: false,
+        onLeave: function(index, direction) {
+            if (direction === 'down'){
+                var loadIndex = index + 1;
+                if (loadIndex < $sections.length) {
+                    $($sections[loadIndex]).removeClass('no-load');
+                }
+            }
+        }
     });
 });
